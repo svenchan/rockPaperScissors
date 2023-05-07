4057da0888c01647ab0f170901a2ef49972d010c
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-07 13:10:20 +0900

    Add .html and .js

---
+++ janken.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+
+}
+
+function playround(playerSelection, Computerselection) {
+
+}
+
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+console.log(playround(playerSelection, computerSelection));
