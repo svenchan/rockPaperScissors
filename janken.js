e0ece08984663622183d5449b5320fc29463f982
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-07 13:54:03 +0900

    Add computerSelection function and playRound function

--- janken.js
+++ janken.js
@@ -1,11 +1,18 @@
 function getComputerChoice() {
-
+const options = ["rock", "paper", "scissors"];
+const randomIndex = Math.floor(Math.random() * options.length);
+return options[randomIndex];
 }
 
-function playround(playerSelection, Computerselection) {
-
-}
+function playRound(playerSelection, Computerselection) {
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+         outcome = "You win! Rock beats scissors";
+    } else {
+            outcome = "code is false";
+         }
+         return outcome;
+    } 
 
 let playerSelection = "rock";
 let computerSelection = getComputerChoice();
-console.log(playround(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
