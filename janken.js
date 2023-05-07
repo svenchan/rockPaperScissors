91676903202ee9123981e860263f2918b52cfa27
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-07 14:20:11 +0900

    Add more options to if statement to increase variety of outcomes

--- janken.js
+++ janken.js
@@ -5,14 +5,22 @@ return options[randomIndex];
 }
 
 function playRound(playerSelection, Computerselection) {
+    let outcome;
     if(playerSelection === "rock" && computerSelection === "scissors"){
          outcome = "You win! Rock beats scissors";
+    } else if(playerSelection === "paper" && computerSelection === "rock"){
+        outcome = "You win! Paper beats rock";
+    } else if(playerSelection === "scissors" && computerSelection === "paper"){
+        outcome = "You win! Scissors beats paper";
     } else {
-            outcome = "code is false";
-         }
-         return outcome;
+        outcome = "You lost!"
+    }
+       return outcome;
     } 
 
-let playerSelection = "rock";
+let playersChoice = "RocK";
+let playerSelection = playersChoice.toLowerCase();
 let computerSelection = getComputerChoice();
+console.log("Player:" + " " + playerSelection);
+console.log("Computer:" + " " + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
