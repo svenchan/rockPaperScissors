b180a6873496997e7c820d005317f3fcede1f1ec
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-09 05:54:05 +0900

    Move let variables inside while loop to make code reachable

--- janken.js
+++ janken.js
@@ -4,30 +4,44 @@ const randomIndex = Math.floor(Math.random() * options.length);
 return options[randomIndex];
 }
 
-function playRound(playerSelection, Computerselection) {
-    let outcome;
-    if(playerSelection === "rock" && computerSelection === "scissors"){
-         outcome = "You win! Rock beats scissors";
-    } else if(playerSelection === "paper" && computerSelection === "rock"){
-        outcome = "You win! Paper beats rock";
-    } else if(playerSelection === "scissors" && computerSelection === "paper"){
-        outcome = "You win! Scissors beats paper";
-    } else {
-        outcome = "You lost!"
-    }
-       return outcome;
-    } 
+let roundResult;
 
-let playersChoice = "RocK";
-let playerSelection = playersChoice.toLowerCase();
-let computerSelection = getComputerChoice();
+function game() {
+    let counter = 0;
+    while (counter < 5) {
+    function playRound(playerSelection, computerselection) {
+        let outcome;
+        if(playerSelection === "rock" && computerSelection === "scissors"){
+            outcome = "You win! Rock beats scissors";
+        } else if(playerSelection === "paper" && computerSelection === "rock"){
+            outcome = "You win! Paper beats rock";
+        } else if(playerSelection === "scissors" && computerSelection === "paper"){
+            outcome = "You win! Scissors beats paper";
+        } else {
+            outcome = "You lost!"
+        }
+        return outcome;
+        } 
+    
+    let playersChoice = prompt("Choose: Rock, paper or scissors");
+    let playerSelection = playersChoice.toLowerCase();
+    let computerSelection = getComputerChoice();
+
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log("Player: " + playerSelection);
+    console.log("Computer:" + " " + computerSelection);
+    console.log(roundResult);
+    counter = counter += 1; 
+    }
+}
+
+game();
 console.log("Player:" + " " + playerSelection);
 console.log("Computer:" + " " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+console.log(roundResult);
+
 
 /* Things to add
-- a prompt that asks for the user input.
-- function game that loops the round function 5 times.
 - a way of keeping the score of rounds won and lost. 
     change the outcome tosomthing that is counatble??
 - a final score anouncement.
