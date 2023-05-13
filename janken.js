1dcb874d15d927ea9c87606a92bef9c0c96b02c1
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-14 06:47:01 +0900

    Rewrite game function. code too messy

--- janken.js
+++ janken.js
@@ -7,97 +7,56 @@ return options[randomIndex];
 let computerScore = 0;
 let playerScore = 0;
 let counter = 0;
+let outcome;
+
+
+function playRound(playerSelection, computerSelection) {
+        
+    
+
+        if(playerSelection === "rock" && computerSelection === "scissors"){
+            return "You win! Rock beats scissors";
+        } else if(playerSelection === "paper" && computerSelection === "rock"){
+            return "You win! Paper beats rock";
+        } else if(playerSelection === "scissors" && computerSelection === "paper"){
+            return "You win! Scissors beats paper";
+        } else if (playerSelection === "scissors" && computerSelection === "scissors") { 
+            return "A draw. Play again";
+          } else if (playerSelection === "rock" && computerSelection === "rock") {
+            return "A draw. Play again";
+          } else if (playerSelection === "paper" && computerSelection === "paper") {
+            return "A draw. Play again";
+          } else {
+            return "You lost!";
+          }
+}
 
 function game() {
-    let roundResult;
+    while (counter < 5){
+        let playersChoice = prompt("Choose: Rock, paper or scissors");
+let playerSelection = playersChoice.toLowerCase();
+let computerSelection = getComputerChoice();
 
+        outcome = playRound(playerSelection, computerSelection);
+        console.log("Player: " + playerSelection);
+        console.log("Computer:" + " " + computerSelection);
+        console.log(outcome);
 
-    while (counter < 5) {
-    function playRound(playerSelection, computerSelection) {
-        let outcome;
-        if(playerSelection === "rock" && computerSelection === "scissors"){
-            outcome = "You win! Rock beats scissors";
-        } else if(playerSelection === "paper" && computerSelection === "rock"){
-            outcome = "You win! Paper beats rock";
-        } else if(playerSelection === "scissors" && computerSelection === "paper"){
-            outcome = "You win! Scissors beats paper";
-        }else if(playerSelection === "scissors" && computerSelection === "scissors"){
-                outcome = "A draw. Play again";
-        }else if(playerSelection === "rock" && computerSelection === "rock"){
-                outcome = "A draw. Play again";
-        }else if(playerSelection === "paper" && computerSelection === "paper"){
-                outcome = "A draw. Play again";
-        } else {
-            outcome = "You lost!"
+        if (outcome.includes("win")){
+        playerScore++;
+        } else if (outcome.includes("lost")){
+        computerScore++;
+        } else if (outcome.includes("draw")){
+
         }
-
-        } 
-    
-    let playersChoice = prompt("Choose: Rock, paper or scissors");
-    let playerSelection = playersChoice.toLowerCase();
-    let computerSelection = getComputerChoice();
-    
-    roundResult = playRound();
-
-    roundResult = playRound(playerSelection, computerSelection);
-    console.log("Player: " + playerSelection);
-    console.log("Computer:" + " " + computerSelection);
-    console.log(roundResult);
- 
-    if (/win/.test(roundResult)){
-	playerScore++;
-	} else if (/lost/.test(roundResult)){
-	computerScore++;
-	}
-    console.log("player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-    counter++; 
-
+        
+        console.log("player score: " + playerScore);
+        console.log("Computer score: " + computerScore);
+        counter++;
     }
 
-    let gameResult = {
-        outcome: roundResult,
-        roundsPlayed: counter
-        };
-
-    return gameResult;
 }
 
-function startGame(){
-    let gameResult = game();
-
-    console.log("GAME OVER");
-    console.log("player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-
-    if (playerScore > computerScore){
-        console.log("The winner is PLAYER");
-    } else if (playerScore < computerScore){
-        console.log("The winner is COMPUTER");
-    } else {
-        console.log("It's a draw");
-    }
+game();
 
 
-    if (gameResult.roundsPlayed === 5){
-        let playAgain = confirm("Do you want to play another game?");
-
-        if (playAgain){
-            playerScore = 0;
-            computerScore = 0;
-            startGame();
-        } else {
-            alert("Thank you for playing");
-        }  
-    }
-}
-
-startGame();
-
-
-
-
-
-/* Things to add
-
-- a prompt to start a new game. */
