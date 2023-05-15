dae25e5dea3d6d592d7a6eef3d1ed81e037017f6
svenchan <122184120+svenchan@users.noreply.github.com>
2023-05-15 16:14:01 +0900

    Add restart function

--- janken.js
+++ janken.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
-const options = ["rock", "paper", "scissors"];
-const randomIndex = Math.floor(Math.random() * options.length);
-return options[randomIndex];
+    const options = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
 }
 
 let computerScore = 0;
@@ -12,30 +12,28 @@ let outcome;
 
 function playRound(playerSelection, computerSelection) {
         
-    
-
-        if(playerSelection === "rock" && computerSelection === "scissors"){
-            return "You win! Rock beats scissors";
-        } else if(playerSelection === "paper" && computerSelection === "rock"){
-            return "You win! Paper beats rock";
-        } else if(playerSelection === "scissors" && computerSelection === "paper"){
-            return "You win! Scissors beats paper";
-        } else if (playerSelection === "scissors" && computerSelection === "scissors") { 
-            return "A draw. Play again";
-          } else if (playerSelection === "rock" && computerSelection === "rock") {
-            return "A draw. Play again";
-          } else if (playerSelection === "paper" && computerSelection === "paper") {
-            return "A draw. Play again";
-          } else {
-            return "You lost!";
-          }
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock beats scissors";
+    } else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper beats rock";
+    } else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors beats paper";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") { 
+        return "A draw. Play again";
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        return "A draw. Play again";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "A draw. Play again";
+    } else {
+        return "You lost!";
+    }
 }
 
 function game() {
     while (counter < 5){
         let playersChoice = prompt("Choose: Rock, paper or scissors");
-let playerSelection = playersChoice.toLowerCase();
-let computerSelection = getComputerChoice();
+        let playerSelection = playersChoice.toLowerCase();
+        let computerSelection = getComputerChoice();
 
         outcome = playRound(playerSelection, computerSelection);
         console.log("Player: " + playerSelection);
@@ -46,17 +44,28 @@ let computerSelection = getComputerChoice();
         playerScore++;
         } else if (outcome.includes("lost")){
         computerScore++;
-        } else if (outcome.includes("draw")){
-
         }
         
         console.log("player score: " + playerScore);
         console.log("Computer score: " + computerScore);
         counter++;
+
+        if (counter === 5){
+            reStartGame();
+        }
     }
 
 }
 
+function reStartGame() {
+  let restart = prompt("Dou want to play another game?");
+  if (restart === "yes"){
+    counter = 0;
+    playerScore = 0;
+    computerScore = 0;
+  }
+
+}
 game();
 
 
