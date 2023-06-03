258140aa7becb62ba04e380b1eb0af4b6114436b
svenchan <122184120+svenchan@users.noreply.github.com>
2023-06-04 05:30:00 +0900

    add promise and promise.raceto wait for player input

--- janken.js
+++ janken.js
@@ -1,14 +1,12 @@
 
-    
+ // Computers coice   
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
 
-
-
-
+//
 function playRound(playerSelection, computerSelection) {
         
     if(playerSelection === "rock" && computerSelection === "scissors"){
@@ -28,13 +26,27 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+// Promise that checks for a click on element and sends back id. 
+
+function waitForButtonClick (element) {
+    return new Promise(resolve => {
+        element.addEventListener("click", function handleClick() {
+            //return element id
+            resolve(element.id);
+            //remove event listener
+            element.removeEvenlistener("click", handleClick);
+        });
+    });
+}
+
+    //Start Game function
+async function game() {
     let outcome;
     let counter = 0;
     var computerScore = 0;
     var playerScore = 0;
 
-    
+    //Score update
     var computerScoreElement = document.getElementById("computer-score");
     var playerScoreElement = document.getElementById("player-score");
     var outcomeNotification = document.getElementById("message-div");
@@ -44,7 +56,28 @@ function game() {
     outcomeNotification.textContent = "";
 
     while (counter < 5){
-        let playerSelection = prompt("Choose: Rock, paper or scissors").toLocaleLowerCase();
+        let playerSelection = "";
+        let rock = document.getElementById("rock-btn");
+        let paper = document.getElementById("paper-btn");
+        let scissors = document.getElementById("scissors-btn");
+        
+        //waits till a button is clicked then takes it's Id as a value
+        let clickedElementId = await Promise.race([
+            waitForButtonClick(rock),
+            waitForButtonClick(paper),
+            waitForButtonClick(scissors)
+        ]);
+
+        if (clickedElementId === "rock-btn") {
+            playerSelection = "rock";
+        } else if (clickedElementId === "paper-btn") {
+            playerSelection = "paper";
+        } else if (clickedElementId === "scissors-btn") {
+            playerSelection = "scissors";
+        }
+
+        
+        //let playerSelection = prompt("Choose: Rock, paper or scissors").toLocaleLowerCase();
         let computerSelection = getComputerChoice();
 
         let outcome = playRound(playerSelection, computerSelection)
