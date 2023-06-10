afe68fbe18a6143df68bdca95a0a002cc7cca5d2
svenchan <122184120+svenchan@users.noreply.github.com>
2023-06-11 05:02:50 +0900

    Remove old code/ unreachable code

--- janken.js
+++ janken.js
@@ -25,6 +25,7 @@ scissorsBtn.addEventListener("click", function() {
     scissorsSound.play();
 });
 
+
 function updateOutcomeMessage(message) {
     const outcomeNotification = document.getElementById("message-div");
     outcomeNotification.textContent = message;
@@ -36,8 +37,7 @@ function updateScores(playerScore, computerScore){
     computerScoreElement.textContent = computerScore.toString();
     playerScoreElement.textContent = playerScore.toString();
 }
-
- // Computers choice   
+ 
  function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
@@ -67,6 +67,7 @@ function showEndGamePopUp(winner){
     }
 }
 
+//Listens for a click event and retrieves id of clicked element
 function waitForButtonClick(element) {
     return new Promise(resolve => {
         element.addEventListener("click", function handleClick() {
@@ -77,7 +78,6 @@ function waitForButtonClick(element) {
 
 function playRound(playerSelection, computerSelection) {
     const computerChoiceDisplay = document.getElementById("computer-choice");
-    const outcomeMessage = document.getElementById("message-div");
 
     let message = "";
     let winner = 0;
@@ -123,6 +123,7 @@ async function game() {
             });
         });
 
+        //waits for first button clicked and sends back the id
         await Promise.race([
             waitForButtonClick(document.getElementById("rock-btn")),
             waitForButtonClick(document.getElementById("paper-btn")),
@@ -147,170 +148,4 @@ async function game() {
 
 }
 
-    /*let computerChoice = getComputerChoice();
-    let computerChoiceDisplay = document.getElementById("computer-choice");
-
-function playRound(playerSelection, computerSelection) {
     
-    if(playerSelection === "rock" && computerSelection === "scissors"){
-        computerChoiceDisplay.src = "images/scissors.png";
-        return "You win! Rock beats scissors";
-    } else if(playerSelection === "paper" && computerSelection === "rock"){
-        computerChoiceDisplay.src = "images/rock1.png";
-        return "You win! Paper beats rock";
-    } else if(playerSelection === "scissors" && computerSelection === "paper"){
-        computerChoiceDisplay.src = "images/paper1.png";
-        return "You win! Scissors beats paper";
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") { 
-        computerChoiceDisplay.src = "images/scissors.png";
-        return "A draw. Play again";
-    } else if (playerSelection === "rock" && computerSelection === "rock") {
-        computerChoiceDisplay.src = "images/rock1.png";
-        return "A draw. Play again";
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        computerChoiceDisplay.src = "images/paper1.png";
-        return "A draw. Play again";
-    } else {
-        return "You lost!";
-    }
-    
-}
-
-// Promise that checks for a click on element and sends back id. 
-
-function waitForButtonClick (element) {
-    return new Promise(resolve => {
-        element.addEventListener("click", function handleClick() {
-            //return element id
-            resolve(element.id);
-            //remove event listener
-            element.removeEventListener("click", handleClick);
-        });
-    });
-}
-
-var startBtn = document.getElementById("start-game-btn");
-var startSound = document.getElementById("bell-sound");
-
-startBtn.addEventListener("click", function() {
-    startSound.play();
-});
-
-let rockBtn = document.getElementById("rock-btn");
-let rockSound = document.getElementById("rock-sound");
-rockBtn.addEventListener("click", function() {
-    rockSound.play();
-});
-
-let paperBtn = document.getElementById("paper-btn");
-let paperSound = document.getElementById("paper-sound");
-paperBtn.addEventListener("click", function() {
-    paperSound.play();
-});
-
-let scissorsBtn = document.getElementById("scissors-btn");
-let scissorsSound = document.getElementById("scissors-sound");
-scissorsBtn.addEventListener("click", function() {
-    scissorsSound.play();
-});
-
-
-    //Start Game function
-async function game() {
-    let counter = 0;
-    var computerScore = 0;
-    var playerScore = 0;
-
-    //Score update
-    var computerScoreElement = document.getElementById("computer-score");
-    var playerScoreElement = document.getElementById("player-score");
-    computerScoreElement.textContent = computerScore.toString();
-    playerScoreElement.textContent = playerScore.toString();
-    
-    while (counter < 5){
-        let playerSelection = "";
-
-        var buttons = document.querySelectorAll(".player-buttons");
-
-        function handleClick(event){
-            //targets button that trigered the event
-            var clickedButton = event.target;
-
-            //removes class from all buttons
-            buttons.forEach(function(button){
-                button.classList.remove("button-clicked");
-            })
-            //Ads class to clicked button
-            clickedButton.classList.add("button-clicked");
-        }
-
-        buttons.forEach(function(button){
-            button.addEventListener("click", handleClick)
-        });
-
-        let rock = document.getElementById("rock-btn");
-        let paper = document.getElementById("paper-btn");
-        let scissors = document.getElementById("scissors-btn");
-       
-        
-        // Player input
-
-        //waits till a button is clicked then takes it's Id as a value
-        let clickedElementId = await Promise.race([
-            waitForButtonClick(rock),
-            waitForButtonClick(paper),
-            waitForButtonClick(scissors)
-        ]);
-
-        if (clickedElementId === "rock-btn") {
-            playerSelection = "rock";
-        } else if (clickedElementId === "paper-btn") {
-            playerSelection = "paper";
-        } else if (clickedElementId === "scissors-btn") {
-            playerSelection = "scissors";
-        }
-
-        
-        let computerSelection = getComputerChoice();
-        let outcome = playRound(playerSelection, computerSelection);
-        var outcomeNotification = document.getElementById("message-div");
-        outcomeNotification.textContent = outcome;
-
-        // Score update
-        if (outcome.includes("win")){
-        playerScore++;
-        } else if (outcome.includes("lost")){
-        computerScore++;
-        }
-        counter++;
-        computerScoreElement.textContent = computerScore.toString();
-        playerScoreElement.textContent = playerScore.toString();
-        
-        if (counter === 5){
-            var popUpContainer = document.getElementById("pop-up-container");
-            popUpContainer.style.display = "flex";
-
-            var closeButton = document.getElementById("close-pop-up");
-            var popUpContent = document.getElementById("pop-up-content");
-            
-            closeButton.addEventListener("click", function() {
-                popUpContainer.style.display = "none";
-            });
-
-            let popUpImg = document.getElementById("pop-up-img");
-            if (playerScore < computerScore){
-                popUpImg.src = "images/computerWin2.jpg"
-                popUpContent.textContent = "You lost. The computer is the winner";
-            } else if (playerScore > computerScore) {
-                popUpContent.textContent = "You won! You beat the computer";
-                popUpImg.src = "images/playerWin2.jpg"
-            } else if (playerScore === computerScore){
-                popUpContent.textContent = "This game is a draw";
-                popUpImg.src = "images/draw.jpg"
-            }
-        }
-    }
-}
-*/
-
-
